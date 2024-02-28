d0a268eee02c103d13f6cd4348e82038e239ade5
WiseVizo <mecoc1011@gmail.com>
2024-02-28 10:52:09 +0530

    login implemented

--- DCRM/dcrm/website/views.py
+++ DCRM/dcrm/website/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
-def home(req):
-    return render(req, "home.html", {})
+def home(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            messages.error(request, 'Logged in successfully')
+             # Clear all previous messages
+            storage = messages.get_messages(request)
+            storage.used = True
+            return redirect('home')  # Redirect to a success page
+        else:
+            messages.error(request, 'Invalid username or password.')
+             # Clear all previous messages
+            storage = messages.get_messages(request)
+            storage.used = True
+    return render(request, "home.html", {})
+
+def login_user(req):
+     return render(req, "home.html", {})
+
+def logout_user(req):
+    pass
