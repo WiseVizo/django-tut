4c83dab35568cf1a1629992e3027accc4acc7f34
WiseVizo <mecoc1011@gmail.com>
2024-02-29 12:25:31 +0530

    ?

--- DCRM/dcrm/website/views.py
+++ DCRM/dcrm/website/views.py
@@ -31,3 +31,5 @@ def logout_user(request):
     messages.success(request,"you have logged out!")
     return redirect("home")
 
+def register_user(request):
+    return render(request, "register.html", {})
